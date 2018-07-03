759d4cebedb2310aebaddd91fd67d00472721579
croef <changrong185@gmail.com>
2018-07-03 20:26:05 +0800

    add run.sh

---
+++ run.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+start() {
+    nvidia-docker start tensorflow
+}
+
+stop() {
+    nvidia-docker stop tensorflow
+}
+
+run() {
+	nvidia-docker run -d \
+	-p 10888:8888 \
+	-p 6006:6006 \
+	-v \
+	/home/webapps/deep-learning/workspace:/notebooks/workspace \
+	--name tensorflow \
+	gcr.io/tensorflow/tensorflow:latest-gpu-py3
+}
+
+case "$1" in
+        run|start|stop)
+		  COMMAND=$1
+		  shift
+		  $COMMAND $@
+		  ;;
+		*)
+		  echo $"Usage:$0 {run|start|stop}"
+	      exit 2
+esac
